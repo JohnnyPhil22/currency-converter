9f12859ff09edd6753fce85acf77b75cb46e534a
JohnnyPhil22 <johnphilips.995@gmail.com>
2023-11-15 11:58:23 +0000

    Fixed the currency conversion errors

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 document.addEventListener("DOMContentLoaded", function () {
     const apiKey = "5ab15174a77b4f34ab10fe3fb60c1885";
-    const apiUrl = "https://open.er-api.com/v6/latest/$5ab15174a77b4f34ab10fe3fb60c1885";
+    const apiUrl = `https://openexchangerates.org/api/latest.json?app_id=5ab15174a77b4f34ab10fe3fb60c1885`;
 
     const amountInput = document.getElementById("amount");
     const fromCurrencySelect = document.getElementById("from-currency-select");
     const toCurrencySelect = document.getElementById("to-currency-select");
-    const convertButton = document.getElementById("convert");
+    const convert = document.getElementById("convert");
     const resultParagraph = document.getElementById("result");
 
     fetch(apiUrl)
@@ -25,23 +25,24 @@ document.addEventListener("DOMContentLoaded", function () {
 
                 fromCurrencySelect.add(option1);
                 toCurrencySelect.add(option2);
+
+				convert.addEventListener("click", function () {
+					const amount = parseFloat(amountInput.value);
+					const fromCurrency = fromCurrencySelect.value;
+					const toCurrency = toCurrencySelect.value;
+
+					if (isNaN(amount)) {
+						resultParagraph.textContent = "Please enter a valid amount.";
+						return;
+					}
+
+					const conversionRate = data.rates[toCurrency] / data.rates[fromCurrency];
+					const result = amount * conversionRate;
+
+					resultParagraph.textContent = `${amount} ${fromCurrency} = ${result.toFixed(2)} ${toCurrency}`;
+				});
+				
             });
         })
         .catch((error) => console.error("Error fetching currency rates:", error));
-
-    convertButton.addEventListener("click", function () {
-        const amount = parseFloat(amountInput.value);
-        const fromCurrency = fromCurrencySelect.value;
-        const toCurrency = toCurrencySelect.value;
-
-        if (isNaN(amount)) {
-            resultParagraph.textContent = "Please enter a valid amount.";
-            return;
-        }
-
-        const conversionRate = data.rates[toCurrency] / data.rates[fromCurrency];
-        const result = amount * conversionRate;
-
-        resultParagraph.textContent = `${amount} ${fromCurrency} = ${result.toFixed(2)} ${toCurrency}`;
-    });
 });
