662206d780daefbb4be815ccdfca12415d1fb59c
JohnnyPhil22 <johnphilips.995@gmail.com>
2023-11-15 18:17:33 +0000

    Minor Changes

--- script.js
+++ script.js
@@ -1,32 +1,32 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const apiKey = "5ab15174a77b4f34ab10fe3fb60c1885";
-    const apiUrl = `https://openexchangerates.org/api/latest.json?app_id=5ab15174a77b4f34ab10fe3fb60c1885`;
+document.addEventListener("DOMContentLoaded", function() {
+	const apiKey = "5ab15174a77b4f34ab10fe3fb60c1885";
+	const apiUrl = `https://openexchangerates.org/api/latest.json?app_id=5ab15174a77b4f34ab10fe3fb60c1885`;
 
-    const amountInput = document.getElementById("amount");
-    const fromCurrencySelect = document.getElementById("from-currency-select");
-    const toCurrencySelect = document.getElementById("to-currency-select");
-    const convert = document.getElementById("convert");
-    const resultParagraph = document.getElementById("result");
+	const amountInput = document.getElementById("amount");
+	const fromCurrencySelect = document.getElementById("from-currency-select");
+	const toCurrencySelect = document.getElementById("to-currency-select");
+	const convert = document.getElementById("convert");
+	const resultParagraph = document.getElementById("result");
 
-    fetch(apiUrl)
-        .then((response) => response.json())
-        .then((data) => {
-            const currencies = Object.keys(data.rates);
+	fetch(apiUrl)
+		.then((response) => response.json())
+		.then((data) => {
+			const currencies = Object.keys(data.rates);
 
-            currencies.forEach((currency) => {
-                const option1 = document.createElement("option");
-                const option2 = document.createElement("option");
+			currencies.forEach((currency) => {
+				const option1 = document.createElement("option");
+				const option2 = document.createElement("option");
 
-                option1.value = currency;
-                option1.text = currency;
+				option1.value = currency;
+				option1.text = currency;
 
-                option2.value = currency;
-                option2.text = currency;
+				option2.value = currency;
+				option2.text = currency;
 
-                fromCurrencySelect.add(option1);
-                toCurrencySelect.add(option2);
+				fromCurrencySelect.add(option1);
+				toCurrencySelect.add(option2);
 
-				convert.addEventListener("click", function () {
+				convert.addEventListener("click", function() {
 					const amount = parseFloat(amountInput.value);
 					const fromCurrency = fromCurrencySelect.value;
 					const toCurrency = toCurrencySelect.value;
@@ -41,8 +41,8 @@ document.addEventListener("DOMContentLoaded", function () {
 
 					resultParagraph.textContent = `${amount} ${fromCurrency} = ${result.toFixed(2)} ${toCurrency}`;
 				});
-				
-            });
-        })
-        .catch((error) => console.error("Error fetching currency rates:", error));
+
+			});
+		})
+		.catch((error) => console.error("Error fetching currency rates:", error));
 });
